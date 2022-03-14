19615965160106058e3c0753464a0236af9f31d0
Victor Egito <victoregito@gmail.com>
2022-03-14 15:34:58 -0400

    teste

--- src/dao/postgres/ReservaDAOImp.java
+++ src/dao/postgres/ReservaDAOImp.java
@@ -1,9 +1,38 @@
 package dao.postgres;
+import Entidades.Reserva;
+import dao.ReservaDAO;
+import java.util.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 /**
  *
  * @author Victor
  */
 public class ReservaDAOImp {
+    
+    private Connection con;
+    
+    private void abreConexao() {
+        try {
+            con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "123");
+        } catch (SQLException ex) {
+            Logger.getLogger(RestauranteDAOImp.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    private void fechaConexao() {
+        try {
+            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(RestauranteDAOImp.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     
 }
